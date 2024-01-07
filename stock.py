213d0a24ec27a2e1c6a42c5506c001122cd05e6f
yaopan666 <155442410+yaopan666@users.noreply.github.com>
2024-01-08 03:06:24 +0800

    Add files via upload

---
+++ stock.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 18 10:15:34 2023
+
+@author: wp
+"""
+import streamlit as st
+import time
+import akshare as ak
+
+
+@st.cache_data
+# 获取股票实时行情数据
+def get_stock_data():
+    data = ak.bond_zh_hs_cov_spot()
+    return data
+
+st.title('股票实时行情')
+
+# 在页面上显示实时数据
+while True:
+    stock_data = get_stock_data()
+    st.write(stock_data)
+    time.sleep(5)  # 每5秒刷新一次数据
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
