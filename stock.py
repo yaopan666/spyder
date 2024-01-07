5dc971f05c762d82c2d4fddc1b56d0368f81279f
yaopan666 <155442410+yaopan666@users.noreply.github.com>
2024-01-08 04:15:53 +0800

    Update stock.py

--- stock.py
+++ stock.py
@@ -10,12 +10,14 @@ st.title('可转债实时行情')
 # 创建一个空白的占位符
 stock_data_placeholder = st.empty()
 
+
 # 累计刷新次数
 refresh_count = 0
 refresh_count_placeholder = st.empty()  # 创建一个用于显示刷新次数的占位符
 
 while True:
     stock_data = ak.bond_zh_hs_cov_spot()  # 获取最新数据
+    stock_data['trade'] = pd.to_numeric(stock_data['trade'], errors='coerce')
     stock_data_placeholder.dataframe(stock_data)  # 使用占位符以DataFrame形式显示最新数据
     refresh_count += 1  # 累计刷新次数加一
     refresh_count_placeholder.text("累计刷新次数: {}".format(refresh_count))  # 更新累计刷新次数的显示
