42fc0e1ce429184a79e60071f9df48f68a8ac721
yaopan666 <155442410+yaopan666@users.noreply.github.com>
2024-01-08 03:20:27 +0800

    Update stock.py

--- stock.py
+++ stock.py
@@ -1,24 +1,21 @@
+# -*- coding: utf-8 -*-
 import streamlit as st
 import time
 import akshare as ak
 
 
-@st.cache
-
-# 获取可转债实时行情数据
+@st.cache_data
+# 获取股票实时行情数据
 def get_stock_data():
     data = ak.bond_zh_hs_cov_spot()
     return data
 
 st.title('可转债实时行情')
 
-# 显示实时数据的函数
-
-def show_stock_data(stock_data):
-    st.write(stock_data)
-
-# 实时更新数据并展示
+# 在页面上显示实时数据
 while True:
     stock_data = get_stock_data()
-    show_stock_data(stock_data)
+    st.write(stock_data)
     time.sleep(5)  # 每5秒刷新一次数据
+
+
